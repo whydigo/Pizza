a88db5565bf5842594ca00263c1c1b73439b5541
whydigo <ukunsss7@gmail.com>
2023-08-10 21:31:39 +0300

    создание компонента пицц и категории, рендер списка

---
+++ src/components/Categories.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => {
+          return (
+            <li
+              onClick={() => setActiveIndex(i)}
+              className={activeIndex === i ? "active" : ""}
+            >
+              {value}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
