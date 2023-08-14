7d9e9de19428ac1c6d19ca28db472104944b5c42
whydigo <ukunsss7@gmail.com>
2023-08-14 21:56:03 +0300

    mockapi, fetch на получение даты

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
 import Categories from "./components/Categories";
 import Header from "./components/Header";
 import PizzaBlock from "./components/PizzaBlock";
 import Sort from "./components/Sort";
 
-import pizzas from "./assets/pizzas.json";
-
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://64da769fe947d30a260b4b54.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -18,10 +29,8 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((obj) => (
-                <PizzaBlock
-                  {...obj}
-                />
+              {items.map((obj) => (
+                <PizzaBlock {...obj} />
               ))}
             </div>
           </div>
